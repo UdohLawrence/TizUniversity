e28a35202e82a8ed04a726986e59297b7dc9b7ce
UdohLawrence <lawrenceaudoh@gmail.com>
2025-08-14 19:52:02 +0100

    update the database context class

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TizUniversity.Models;
+
+namespace TizUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TizUniversity.Models.Student> Student { get; set; } = default!;
+    }
+}

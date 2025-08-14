b90754fa6fce00f4e02ec9237362a6600b2acbeb
UdohLawrence <lawrenceaudoh@gmail.com>
2025-08-14 19:49:57 +0100

    scaffold student pages

---
+++ Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TizUniversity.Data;
+using TizUniversity.Models;
+
+namespace TizUniversity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly TizUniversity.Data.SchoolContext _context;
+
+        public DetailsModel(TizUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (student is not null)
+            {
+                Student = student;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
